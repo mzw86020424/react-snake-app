7fd259d8677b0ea2959800d601675159835a2d2d
mzw86020424 <mzw86020424@gmail.com>
2021-03-15 10:54:38 +0900

    ゲームの状態（start, gemeover）の実装

--- src/App.js
+++ src/App.js
@@ -9,6 +9,13 @@ const initialPosition = { x: 17, y: 17 }
 const initialValues = initFields(35, initialPosition)
 const defaultInterval = 100
 
+const GameStatus = Object.freeze({
+  init: 'init',
+  playing: 'playing',
+  suspended: 'suspended',
+  gameover: 'gameover',
+})
+
 let timer = undefined
 
 const unsubscribe = () => {
@@ -18,9 +25,20 @@ const unsubscribe = () => {
   clearInterval(timer)
 }
 
+const isCollision = (fieldSize, position) => {
+  if (position.y < 0 || position.x < 0) {
+    return true;
+  }
+  if (position.y > fieldSize - 1 || position.x > fieldSize - 1) {
+    return true;
+  }
+  return false;
+}
+
 function App() {
   const [fields, setFields] = useState(initialValues)
   const [position, setPosition] = useState()
+  const [status, setStatus] = useState(GameStatus.init)
   const [tick, setTick] = useState(0)
 
   useEffect(() => {
@@ -33,19 +51,39 @@ function App() {
   }, [])
 
   useEffect(() => {
-    if (!position) {
+    if (!position || status !== GameStatus.playing) {
       return
     }
-    goUp()
+    const canContinue = goUp() // goUpを実行、boolean値が返る
+    if (!canContinue) { // goUpがfalseならゲームを終了させる
+      unsubscribe()
+      setStatus(GameStatus.gameover)
+    }
   }, [tick]) //tickに変更があるたびに実行
+
+  const onStart = ()=>setStatus(GameStatus.playing)
+
+  const onRestart = () => {
+    timer = setInterval(() => {
+      setTick(tick => tick + 1)
+    }, defaultInterval)
+    // setDirection(Direction.up) //????
+    setStatus(GameStatus.init)
+    setPosition(initialPosition)
+    setFields(initFields(35, initialPosition))
+  }
 
   const goUp = () => {
     const { x, y } = position
-    const nextY = Math.max(y - 1, 0)
+    const newPosition = { x, y: y - 1 }
+    if (isCollision(fields.length, newPosition)) { //次のポジションがフィールド外でないかisCollisionで判定
+      return false
+    }
     fields[y][x] = ''
-    fields[nextY][x] = 'snake'
-    setPosition({ x, y: nextY })
+    fields[newPosition.y][x] = 'snake'
+    setPosition(newPosition)
     setFields(fields)
+    return true
   }
 
   return (
@@ -63,7 +101,7 @@ function App() {
       </main>
 
       <footer className="footer">
-        <Button />
+        <Button status={status} onStart={onStart} onRestart={onRestart} />
         <ManipulationPanel />
       </footer>
       
